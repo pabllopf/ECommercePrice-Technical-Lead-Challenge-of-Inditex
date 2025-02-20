94964f30f96475333e47abecf9ee51ad879e1c6c
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-02-20 09:56:34 +0100

    feature: add simple solution

---
+++ ECommercePrice/src/main/java/dev/pabllopf/ecommerceprice/ECommercePriceApplication.java
@@ -0,0 +1,13 @@
+package dev.pabllopf.ecommerceprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercePriceApplication.class, args);
+    }
+
+}
---
+++ ECommercePrice/src/test/java/dev/pabllopf/ecommerceprice/ECommercePriceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pabllopf.ecommerceprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
