1b41dfc11b0e3141e29892f19638c13c60034765
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-02-20 16:15:32 +0100

    fix: external access to h2 panel admin

--- src/main/java/dev/pabllopf/ecommerceprice/infrastructure/config/SecurityConfig.java
+++ src/main/java/dev/pabllopf/ecommerceprice/infrastructure/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/login",
