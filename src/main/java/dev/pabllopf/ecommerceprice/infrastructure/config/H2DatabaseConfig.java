e19f4c32caf30aa1f33ba91eb8cd97bcd7de9b00
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-02-21 20:46:58 +0100

    docs: H2DatabaseConfig

--- src/main/java/dev/pabllopf/ecommerceprice/infrastructure/config/H2DatabaseConfig.java
+++ src/main/java/dev/pabllopf/ecommerceprice/infrastructure/config/H2DatabaseConfig.java
@@ -8,13 +8,19 @@ import javax.sql.DataSource;
 @Configuration
 public class H2DatabaseConfig {
 
+    /**
+     * Configures a DataSource bean for connecting to an H2 in-memory database.
+     * This configuration is used for testing or development purposes.
+     *
+     * @return A DataSource object configured to connect to the H2 in-memory database.
+     */
     @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .url("jdbc:h2:mem:ecommerceprice;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
-                .driverClassName("org.h2.Driver")
-                .username("sa")
-                .password("")
-                .build();
+        return DataSourceBuilder.create() // Creates a new DataSourceBuilder instance
+                .url("jdbc:h2:mem:ecommerceprice;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE") // URL for the in-memory H2 database
+                .driverClassName("org.h2.Driver") // The driver class name for H2
+                .username("sa") // Username for the database connection
+                .password("") // Password for the database connection (empty in this case)
+                .build(); // Builds and returns the DataSource instance
     }
 }
