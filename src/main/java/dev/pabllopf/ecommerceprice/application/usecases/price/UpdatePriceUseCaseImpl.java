be28c1e50d7f4ef0b16c166a7e8dbb0ef969a851
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-02-21 20:39:49 +0100

    docs: UpdatePriceUseCaseImpl

--- src/main/java/dev/pabllopf/ecommerceprice/application/usecases/price/UpdatePriceUseCaseImpl.java
+++ src/main/java/dev/pabllopf/ecommerceprice/application/usecases/price/UpdatePriceUseCaseImpl.java
@@ -5,13 +5,25 @@ import dev.pabllopf.ecommerceprice.domain.ports.in.price.IUpdatePriceUseCase;
 import dev.pabllopf.ecommerceprice.domain.ports.out.IPriceRepositoryPort;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Implementation of the use case for updating a price.
+ * This class handles the logic of updating an existing price record in the repository.
+ */
 @RequiredArgsConstructor
 public class UpdatePriceUseCaseImpl implements IUpdatePriceUseCase {
 
+    // The repository interface responsible for interacting with the data storage (e.g., database)
     private final IPriceRepositoryPort priceRepositoryPort;
 
+    /**
+     * Updates the details of an existing price.
+     * This method sends the updated price information to the repository for storage.
+     *
+     * @param price The price object containing the updated details.
+     */
     @Override
     public void updatePrice(Price price) {
+        // Update the price using the repository's update method
         priceRepositoryPort.update(price);
     }
 }
