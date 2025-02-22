778e90da12f46dff769d47b14e862465e0aef94e
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-02-22 13:55:47 +0100

    test: IDeletePriceUseCaseTest

---
+++ src/test/java/dev/pabllopf/ecommerceprice/domain/ports/in/price/IDeletePriceUseCaseTest.java
@@ -0,0 +1,39 @@
+package dev.pabllopf.ecommerceprice.domain.ports.in.price;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+public class IDeletePriceUseCaseTest {
+
+    @Mock
+    private IDeletePriceUseCase deletePriceUseCase;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testDeletePriceSuccess() {
+        Long priceId = 1L;
+
+        deletePriceUseCase.deletePrice(priceId);
+
+    }
+
+    @Test
+    public void testDeletePriceNotFound() {
+        Long priceId = 1L;
+
+        doThrow(new IllegalArgumentException("Price not found")).when(deletePriceUseCase).deletePrice(priceId);
+
+        assertThrows(IllegalArgumentException.class, () -> deletePriceUseCase.deletePrice(priceId));
+    }
+}
