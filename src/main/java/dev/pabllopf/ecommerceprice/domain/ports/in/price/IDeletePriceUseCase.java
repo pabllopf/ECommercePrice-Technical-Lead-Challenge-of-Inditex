b6bc55e1b387290ccff5c44d890bfb7af4add252
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-02-22 12:55:25 +0100

    docs: IDeletePriceUseCase

--- src/main/java/dev/pabllopf/ecommerceprice/domain/ports/in/price/IDeletePriceUseCase.java
+++ src/main/java/dev/pabllopf/ecommerceprice/domain/ports/in/price/IDeletePriceUseCase.java
@@ -1,5 +1,22 @@
 package dev.pabllopf.ecommerceprice.domain.ports.in.price;
 
+/**
+ * IDeletePriceUseCase defines the contract for the use case of deleting an existing price record.
+ * This interface serves as the entry point for the application layer to interact with the core business logic related to price deletion.
+ * It is part of the domain layer and abstracts the details of how a price record is deleted from the system.
+ *
+ * By following this approach, the business logic for deleting prices is decoupled from infrastructure or persistence logic,
+ * ensuring better testability and maintainability.
+ */
 public interface IDeletePriceUseCase {
-    public void deletePrice(Long id);
+
+    /**
+     * Deletes a price record by its ID.
+     * This method processes the deletion of a price record identified by the provided ID.
+     * The actual implementation typically involves interacting with the repository layer to remove the record from the database.
+     *
+     * @param id The ID of the price to be deleted.
+     * @throws IllegalArgumentException if the price with the given ID does not exist.
+     */
+    void deletePrice(Long id);
 }
