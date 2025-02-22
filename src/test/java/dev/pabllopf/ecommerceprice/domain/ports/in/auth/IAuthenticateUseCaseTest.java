4ee85d069b5b924c6b6b8fa95ce7c44b976f0410
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-02-22 13:55:36 +0100

    test: IAuthenticateUseCaseTest

---
+++ src/test/java/dev/pabllopf/ecommerceprice/domain/ports/in/auth/IAuthenticateUseCaseTest.java
@@ -0,0 +1,44 @@
+package dev.pabllopf.ecommerceprice.domain.ports.in.auth;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+public class IAuthenticateUseCaseTest {
+
+    @Mock
+    private IAuthenticateUseCase authenticateUseCase;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testAuthenticateSuccess() {
+        String username = "testUser";
+        String password = "testPassword";
+        String expectedToken = "token";
+        when(authenticateUseCase.authenticate(username, password)).thenReturn(expectedToken);
+
+        String actualToken = authenticateUseCase.authenticate(username, password);
+
+        assertEquals(expectedToken, actualToken);
+    }
+
+    @Test
+    public void testAuthenticateFailure() {
+        String username = "testUser";
+        String password = "wrongPassword";
+
+        when(authenticateUseCase.authenticate(username, password)).thenThrow(new IllegalArgumentException("Invalid credentials"));
+
+        assertThrows(IllegalArgumentException.class, () -> authenticateUseCase.authenticate(username, password));
+    }
+}
